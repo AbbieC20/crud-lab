969f680edab2175111ac3a4daea773bd40d9c27a
Abbie Cornelius <abbyc@sky.com>
2021-12-10 17:35:50 +0000

    commit tree abab7c5981caa67af6149efab0750e2855fdba3c

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 
 class Review extends Component {
 
-  render() {
-    const { review } = this.props;
+  handleOnClick() {
+    this.props.deleteReview(this.props.review.id)
+  }
 
+  render() {
     return (
       <div>
         <li>
-          {review.text}
+          {this.props.review.text}
+          <button onClick={() => this.handleOnClick()}> X </button>
         </li>
-        <button> X </button>
       </div>
     );
   }
